06363e2b2879c356eb17f2f38df54f67c0abda72
Jesus Grajeda <lisoas8@gmail.com>
2021-02-21 22:42:23 -0700

    Practica 4
    New Project

---
+++ TheCheezery/app/src/main/java/grajeda/jesus/thecheezery/MenuActivity.kt
@@ -0,0 +1,11 @@
+package grajeda.jesus.thecheezery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
